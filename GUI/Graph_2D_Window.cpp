775f87f4df27157027f856e251f1652275056c72
monsterjamp <monsterjamp@yahoo.com>
2018-09-25 01:46:00 -0500

    Dialog to enter equation

--- GUI/Graph_2D_Window.cpp
+++ GUI/Graph_2D_Window.cpp
@@ -1,7 +1,14 @@
 #include <QMenuBar>
 #include <QCloseEvent>
+#include <QInputDialog>
+#include <QString>
 
 #include "Graph_2D_Window.h"
+#include "Calculator.h"
+
+#ifdef DEBUG
+  #include <iostream>
+#endif
 
 
 Graph_2D_Window::Graph_2D_Window()
@@ -18,14 +25,28 @@ void Graph_2D_Window::InitMenuBar()
 	const QIcon exit_icon = QIcon::fromTheme("application-exit");
 	
 	QMenu *graph_menu = menuBar()->addMenu(tr("&Graph"));
-	graph_menu->addAction(tr("Enter equation..."), this, &Graph_2D_Window::graph);
+	graph_menu->addAction(tr("Enter equation..."), this, &Graph_2D_Window::enter_equation);
 	
 	graph_menu->addSeparator();
 	
 	graph_menu->addAction(exit_icon, tr("&Quit"), this, &QWidget::close);
 }
 
-void Graph_2D_Window::graph()
+void Graph_2D_Window::enter_equation()
 {
-	// TODO: open dialog
+	bool ok;
+	
+	QString text = QInputDialog::getText(this, tr("Equation"), tr("Enter equation: "), QLineEdit::Normal, tr(""), &ok);
+	
+	#ifdef DEBUG //Output points to console
+		points = Calc::InfixToPoints(text.toStdString(), -10, 10, 1);
+		
+		for(unsigned int i = 0; i < points.size(); ++i) {
+			if(i % 2 == 0) {
+				std::cout << "(" << points[i] << ", ";
+			} else {
+				std::cout << points[i] << ")" << std::endl;
+			}
+		}
+	#endif
 }
--- GUI/Graph_2D_Window.h
+++ GUI/Graph_2D_Window.h
@@ -2,6 +2,10 @@
 
 #include <QMainWindow>
 #include <QOpenGLFunctions>
+#include <QInputDialog>
+
+#include <vector>
+
 
 class Graph_2D_Window : public QMainWindow, protected QOpenGLFunctions
 {
@@ -11,9 +15,11 @@ public:
 protected:
 
 private slots:
-	void graph();
+	void enter_equation();
 	
 private:
 	void InitMenuBar();
+	
+	std::vector<double> points;
 	
 };
